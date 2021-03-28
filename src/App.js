528f917c7dfe5b6f016615f9168f63904f786150
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-03-28 13:11:09 -0400

    working on table layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Header from "./components/Header/header";
-
+import Table from './components/Table/table';
 function App() {
   return (
     <div className="App">
         <Header />
+        <Table />
     </div>
   );
 }
